f3bfe699c3285ef56605afccc76c8e64dcc60fbc
Jelena2112 <dijanic@gmail.com>
2022-04-06 12:09:01 +0200

    Updates

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -13,29 +13,29 @@
         <label>Ime:</label>
         <input type="text" name="name" value="{{ $user->name }}"><br>
 
-        <label >Phone:</label>
+        <label >Telefon:</label>
         <input type="text"  name="phone" value="{{ $user->userInfo->phone }}"><br>
 
-        <label>Your gender:</label>
+        <label>Pol:</label>
         <select name="gender">
             <option value="male"
                     @if($user->userInfo->gender == 'male')
                     selected
-                @endif>Male</option>
+                @endif>Muski</option>
 
             <option value="female"
                     @if($user->userInfo->gender == 'female')
                     selected
-                @endif>Female</option>
-        </select>
+                @endif>Zenski</option>
+        </select><br>
 
-        <label >Date of birth:</label>
+        <label >Datum rodjenja:</label>
         <input type="date" name="date_of_birth" value="{{ $user->userInfo->date_of_birth }}"><br>
 
-        <label >Weight:</label>
+        <label >Tezina:</label>
         <input type="number" name="weight" value="{{ $user->userInfo->weight }}"><br>
 
-        <label >Height:</label>
+        <label >Visina:</label>
         <input type="number" name="height" value="{{ $user->userInfo->height }}"><br>
 
         <input type="submit" value="Submit">
--- resources/views/userInfo.blade.php
+++ resources/views/userInfo.blade.php
@@ -4,14 +4,14 @@
 
     <p>Ime: {{ $user->name }}</p>
     <p>Email: {{ $user->email }}</p>
-    <p>Phone: {{ $user->userInfo->phone ?? "Nije upisan broj telefona"}}</p>
-    <p>Gender: {{ $user->userInfo->gender ?? "Nije upisan pol"}}</p>
-    <p>Date of birth: {{ $user->userInfo->date_of_birth ?? "Nije upisan datum rodjenja"}} </p>
-    <p>Weight: {{ $user->userInfo->weight ?? "Nije upisana tezina" }}</p>
-    <p>Height: {{ $user->userInfo->height ?? "Nije upisana visina"}} </p>
-    <p>Current Teeth: {{ $user->userDentalRecord->current_teeth ?? "Nisu upisani zubi"}}</p>
-    <p>Missing Teeth: {{ $user->userDentalRecord->missing_teeth ?? "Nisu upisani zubi koji nedostaju"}}</p>
-    <p>Notes: {{ $user->userDentalRecord->notes ?? "Nema zapisa beleske"}}</p>
+    <p>Telefon: {{ $user->userInfo->phone ?? "Nije upisan broj telefona"}}</p>
+    <p>Pol: {{ $user->userInfo->gender ?? "Nije upisan pol"}}</p>
+    <p>Datum rodjenja: {{ $user->userInfo->date_of_birth ?? "Nije upisan datum rodjenja"}} </p>
+    <p>Tezina: {{ $user->userInfo->weight ?? "Nije upisana tezina" }}</p>
+    <p>Visina: {{ $user->userInfo->height ?? "Nije upisana visina"}} </p>
+    <p>Trenutni zubi: {{ $user->userDentalRecord->current_teeth ?? "Nisu upisani zubi"}}</p>
+    <p>Zubi koji nedostaju: {{ $user->userDentalRecord->missing_teeth ?? "Nisu upisani zubi koji nedostaju"}}</p>
+    <p>Beleske: {{ $user->userDentalRecord->notes ?? "Nema zapisa beleske"}}</p>
 
     @if( $user->UserMedicalRecord != null )
         <p>Medicinski karton: {{ $user->UserMedicalRecord->notes }}</p>
