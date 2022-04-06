4076a93af29511d96686ab83241a1eae6c3630ec
Jelena2112 <dijanic@gmail.com>
2022-04-06 12:02:24 +0200

    Changed stobatoloski_karton  blade page

--- resources/views/admin/dental.blade.php
+++ resources/views/admin/dental.blade.php
@@ -3,15 +3,35 @@
 @section('content')
     <p>Ime korisnika: {{ $user->name }}</p>
 
+    @if(\Illuminate\Support\Facades\Auth::user()->userType->user_type == 'zubar')
+
     <form action="{{ route('updateDentalRecord') }}" method="post">
         {{ csrf_field() }}
 
         <input type="hidden" value="{{ $user->id }}" name="user_id">
-       <input type="text" value="{{ $user->userDentalRecord->current_teeth }}" name="current_teeth" >
-       <input type="text" value="{{ $user->userDentalRecord->missing_teeth }}" name="missing_teeth">
-       <input type="text" value="{{ $user->userDentalRecord->notes }}" name="notes">
+
+        <label>Trenutni zubi:</label>
+       <input type="text" value="{{ $user->userDentalRecord->current_teeth }}" name="current_teeth" ><br>
+
+        <label>Zubi koji nedostaju:</label>
+       <input type="text" value="{{ $user->userDentalRecord->missing_teeth }}" name="missing_teeth"><br>
+
+        <label>Beleske:</label>
+       <input type="text" value="{{ $user->userDentalRecord->notes }}" name="notes"><br>
         <input type="submit" value="Submit">
+
     </form>
+
+    @else
+
+        <p>Trenutni zubi: {{ $user->userDentalRecord->current_teeth }}</p>
+        <p>Zubi koji nedostaju: {{ $user->userDentalRecord->missing_teeth }}</p>
+        <p>Beleske: {{ $user->userDentalRecord->notes }}</p>
+
+    @endif
+
+
+
 
 
 
